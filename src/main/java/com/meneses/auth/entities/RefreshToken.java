27b69fecace0f7cb83f7c77b9ed98fd355150241
joao_meneses <joaovic2012@hotmail.com>
2026-04-02 17:30:52 -0300

    User e RefreshToken update

--- src/main/java/com/meneses/auth/entities/RefreshToken.java
+++ src/main/java/com/meneses/auth/entities/RefreshToken.java
@@ -1,10 +1,18 @@
 package com.meneses.auth.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,46 +21,4 @@ public class RefreshToken {
     private LocalDateTime expiration;
     @ManyToOne
     private User user;
-
-    public RefreshToken(){
-    }
-
-    public RefreshToken(Long id, String token, LocalDateTime expiration, User user) {
-        Id = id;
-        this.token = token;
-        this.expiration = expiration;
-        this.user = user;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public LocalDateTime getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(LocalDateTime expiration) {
-        this.expiration = expiration;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
--- src/main/java/com/meneses/auth/entities/User.java
+++ src/main/java/com/meneses/auth/entities/User.java
@@ -4,8 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,47 +21,4 @@ public class User {
     private String email;
     private String password;
     private String role;
-
-    public User(){
-
-    }
-
-    public User(long id, String email, String password, String role) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
