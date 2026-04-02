dd28204dda2a864cf7cf58802e5428d877648e54
joao_meneses <joaovic2012@hotmail.com>
2026-04-02 17:12:14 -0300

    Criada entity RefreshToken

---
+++ src/main/java/com/meneses/auth/entity/RefreshToken.java
@@ -0,0 +1,58 @@
+package com.meneses.auth.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String token;
+    private LocalDateTime expiration;
+    @ManyToOne
+    private User user;
+
+    public RefreshToken(){
+    }
+
+    public RefreshToken(Long id, String token, LocalDateTime expiration, User user) {
+        Id = id;
+        this.token = token;
+        this.expiration = expiration;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(LocalDateTime expiration) {
+        this.expiration = expiration;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
