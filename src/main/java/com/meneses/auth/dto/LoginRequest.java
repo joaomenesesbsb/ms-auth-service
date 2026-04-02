9131abfa53212cd670b2fac242674837b5ea2f5c
joao_meneses <joaovic2012@hotmail.com>
2026-04-02 17:28:51 -0300

    LoginRequest update

--- src/main/java/com/meneses/auth/dto/LoginRequest.java
+++ src/main/java/com/meneses/auth/dto/LoginRequest.java
@@ -1,5 +1,13 @@
 package com.meneses.auth.dto;
 
+import com.meneses.auth.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;
